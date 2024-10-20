ea376f422becccb50d693042b52e59df1abbd2a4
Nitin Goyal <goyalnitin92566@gmail.com>
2024-10-20 18:16:47 +0530

    create a ticket price system for different ages

---
+++ ticketprice.py
@@ -0,0 +1,11 @@
+# Take user input
+age = int(input("Enter Your age :"))
+is_student=("Are you a student ? (Yes/No): ")
+if age<=5:
+  print("Your ticket is free!")
+elif age<=13:
+  print("Your ticket is Rs.99/-")
+elif age<=18:
+  print("Your ticket is Rs.149/-")
+else:
+  print("Your ticket is Rs.199/-")
